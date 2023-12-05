44e80d9bd373de099574c324b4734cd41a8c9c78
Baransel <49001016+baranselklnc@users.noreply.github.com>
2023-12-05 17:45:05 +0300

    splashScreen added the project and fixed duration problem

--- app/src/main/java/com/example/logisticlink/splashScreen.kt
+++ app/src/main/java/com/example/logisticlink/splashScreen.kt
@@ -3,28 +3,34 @@ package com.example.logisticlink
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
 class splashScreen : AppCompatActivity() {
-    private val SPLASH_DELAY: Long = 3000 // Milliseconds, 3 saniye örneğin
+    private val SPLASH_DELAY: Long = 5000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        val imageView = findViewById<ImageView>(R.id.gifImageView)
+        val imageView = findViewById<ImageView>(R.id.imageView)
 
-        // Glide kütüphanesini kullanarak GIF'i yükleyin
         Glide.with(this)
             .asGif()
-            .load(R.raw.splashscreenpro) // "your_gif" dosyanızın adını buraya ekleyin
+            .load(R.raw.splashscreenpro)
             .into(imageView)
 
-        // Splash ekranını belirli bir süre gösterdikten sonra ana aktiviteye geçiş yap
-        android.os.Handler().postDelayed({
-            val mainIntent = Intent(this, MainActivity::class.java)
-            startActivity(mainIntent)
-            finish()
-        }, SPLASH_DELAY)
+        object : CountDownTimer(SPLASH_DELAY, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                // Geri sayım sırasında her saniye bir şeyler yapabilirsiniz
+            }
+
+            override fun onFinish() {
+                val mainIntent = Intent(this@splashScreen, MainActivity::class.java)
+                startActivity(mainIntent)
+                finish()
+            }
+        }.start()
     }
 }
