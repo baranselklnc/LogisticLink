3ac0fb78c41b70d4905e7a55b1fce6b45fdb53bf
Baransel <49001016+baranselklnc@users.noreply.github.com>
2023-12-11 11:05:34 +0300

    Homepage.kt  has been uptaded

--- app/src/main/java/com/example/logisticlink/Homepage.kt
+++ app/src/main/java/com/example/logisticlink/Homepage.kt
@@ -44,7 +44,7 @@ class Homepage : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Homepage.
+         * @return A new instance of fragment homepage.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
