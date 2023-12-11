0c99c0ec3acc09e6f8f79e3f9aae0b040b76a69c
Baransel <49001016+baranselklnc@users.noreply.github.com>
2023-12-11 11:08:08 +0300

    "MainActivity will continue to be edited. On the 8.48"

--- app/src/main/java/com/example/logisticlink/MainActivity.kt
+++ app/src/main/java/com/example/logisticlink/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.logisticlink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.logisticlink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.->replaceFragment(Homepage())
+            }
+        }
+
+    }
+
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction=fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+
     }
 }
