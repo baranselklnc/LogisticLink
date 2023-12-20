a78de4c5e49427849d1f0d61a592bbb08b236b77
Baransel <49001016+baranselklnc@users.noreply.github.com>
2023-12-20 17:33:15 +0300

    Hide keyboard function has been created

--- app/src/main/java/com/example/logisticlink/CustomerRegisterMailScreen.kt
+++ app/src/main/java/com/example/logisticlink/CustomerRegisterMailScreen.kt
@@ -1,14 +1,18 @@
 package com.example.logisticlink
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.example.logisticlink.databinding.ActivityCustomerRegisterMailScreenBinding
 
 class CustomerRegisterMailScreen : AppCompatActivity() {
     private lateinit var binding: ActivityCustomerRegisterMailScreenBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         binding= ActivityCustomerRegisterMailScreenBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -16,11 +20,12 @@ class CustomerRegisterMailScreen : AppCompatActivity() {
         val nullErrorMail=binding.nullErrorMail
         val username=intent.getStringExtra("key")
         val customerNameforMail=binding. customerNameforMail
-        customerNameforMail.text="${username} mail hesabına ihtiyacımız var"
+        customerNameforMail.text="$username mail hesabına ihtiyacımız var"
         val firstExpression="@"
         val secondExpression=".com"
         val nextButton=binding.nextButton
     nextButton.setOnClickListener {
+        hideKeyboard()
     val email=binding.customerRegisterMailEdit.text.toString()
     if (email.isEmpty()|| (!email.contains(firstExpression) || !email.contains(secondExpression))){
         nullErrorMail.visibility=View.VISIBLE
@@ -32,4 +37,12 @@ class CustomerRegisterMailScreen : AppCompatActivity() {
 
     }
 }
+    private fun hideKeyboard() {
+        val view: View? = currentFocus
+        if (view != null) {
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
     }
