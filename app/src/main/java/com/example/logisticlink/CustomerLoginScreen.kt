88aec94aa67b0ac312ba66314fd3e77a7431dfb3
Baransel <49001016+baranselklnc@users.noreply.github.com>
2023-12-21 17:40:46 +0300

    intent problem has been countiuned

--- app/src/main/java/com/example/logisticlink/CustomerLoginScreen.kt
+++ app/src/main/java/com/example/logisticlink/CustomerLoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.logisticlink
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,17 +8,29 @@ import com.example.logisticlink.databinding.ActivityCustomerLoginScreenBinding
 
 class CustomerLoginScreen : AppCompatActivity() {
     private lateinit var binding: ActivityCustomerLoginScreenBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityCustomerLoginScreenBinding.inflate(layoutInflater)
+        binding = ActivityCustomerLoginScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        CustomToastManager.showCustomToast(this,"E-posta ve şifrenizi girerek oturum açabilirsiniz")
 
-        val carrierLoginButton=binding.carrierLoginButton
-carrierLoginButton.setOnClickListener {
-    val intent = Intent(this, MainActivity::class.java)
-    startActivity(intent)
-}
+        CustomToastManager.showCustomToast(
+            this,
+            "E-posta ve şifrenizi girerek oturum açabilirsiniz"
+        )
+        val testText = binding.testText
+        val carrierLoginButton = binding.carrierLoginButton
+        val userPassword = intent.getStringExtra("password")
+        val loginmail = intent.getStringExtra("email")
+        testText.text = "Şifre= $userPassword ,Eposta $loginmail "
+
+        carrierLoginButton.setOnClickListener {
+
+
+            // Yeni bir Intent oluşturarak MainActivity'ye geçiş yap
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
-
 }
