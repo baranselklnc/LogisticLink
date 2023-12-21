2081b4859627425f8a942e79e42f2f650e816f4f
Baransel <49001016+baranselklnc@users.noreply.github.com>
2023-12-21 17:41:16 +0300

    Override libraries has been deprecated.

--- app/src/main/java/com/example/logisticlink/CarrierRegisterScreen.kt
+++ app/src/main/java/com/example/logisticlink/CarrierRegisterScreen.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.widget.EditText
 import android.widget.ImageView
