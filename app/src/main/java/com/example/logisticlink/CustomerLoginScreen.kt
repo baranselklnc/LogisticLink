ec164929e267480c0223a24350e296aea2cc01ac
Baransel <49001016+baranselklnc@users.noreply.github.com>
2023-12-22 17:36:43 +0300

    ıntent problem fixed and Dialog Alert created and still working

--- app/src/main/java/com/example/logisticlink/CustomerLoginScreen.kt
+++ app/src/main/java/com/example/logisticlink/CustomerLoginScreen.kt
@@ -4,10 +4,32 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import com.example.logisticlink.databinding.ActivityCustomerLoginScreenBinding
 
 class CustomerLoginScreen : AppCompatActivity() {
     private lateinit var binding: ActivityCustomerLoginScreenBinding
+
+    private fun showCustomDialog() {
+        val builder=AlertDialog.Builder(this)
+        val customView=LayoutInflater.from(this).inflate(R.layout.forget_password_dialog,null)
+        builder.setView(customView)
+        val eMailSendButton=customView.findViewById<Button>(R.id.rePasswordButton)
+        val dialog =builder.create()
+
+        eMailSendButton.setOnClickListener {
+            CustomToastManager.showCustomToast(this,"Yeni şifreniz mailinize gönderildi")
+            dialog.dismiss()
+        }
+        val eMailEditText=customView.findViewById<EditText>(R.id.rePasswordEditText)
+
+        dialog.show()
+    }
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,19 +41,36 @@ class CustomerLoginScreen : AppCompatActivity() {
             this,
             "E-posta ve şifrenizi girerek oturum açabilirsiniz"
         )
+        val carrierLoginMail=binding.carrierLoginMail
+        val carrierLoginPassword=binding.carrierLoginPassword
         val testText = binding.testText
         val carrierLoginButton = binding.carrierLoginButton
+        val forgetPasswordText=binding.forgetPasswordText
+
         val receivedMail=(application as MyApplication).sharedMail
         val receivedPassword=(application as MyApplication).sharedPassword
 
-       testText.text = "$receivedMail,$receivedPassword "
-
         carrierLoginButton.setOnClickListener {
 
+            val mail =carrierLoginMail.text.toString()
+            val password=carrierLoginPassword.text.toString()
+            if (mail!=receivedMail || password!=receivedPassword )
+            {
+                CustomToastManager.showCustomToast(this,"Lütfen kullanıcı adı veya şifrenizi kontrol edin.")
+            }
+            else{
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+            }
+
+            testText.text = "$receivedMail,$receivedPassword "
 
             // Yeni bir Intent oluşturarak MainActivity'ye geçiş yap
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+
+        }
+        forgetPasswordText.setOnClickListener {showCustomDialog()}
+
+
         }
     }
-}
