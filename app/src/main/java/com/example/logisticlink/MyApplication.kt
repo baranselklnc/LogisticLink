b609b98a1555f481fcd26af82196be2e1119d225
Baransel <49001016+baranselklnc@users.noreply.github.com>
2023-12-23 17:29:16 +0300

    new variables added to Application

--- app/src/main/java/com/example/logisticlink/MyApplication.kt
+++ app/src/main/java/com/example/logisticlink/MyApplication.kt
@@ -5,6 +5,8 @@ import android.app.Application
 class MyApplication : Application() {
     var sharedPassword:String?=null
     var sharedMail:String?=null
+    var sharedCarrierMail:String?=null
+    var sharedCarrierPassword:String?=null
     override fun onCreate() {
         super.onCreate()
 
